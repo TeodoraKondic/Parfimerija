305d36088107a21fc7b98767f6d4b60eebfa4fcf
TeodoraKondic <teodorak.ns@gmail.com>
2026-02-15 16:17:11 +0100

    Isparvaljena greska kod hear_btn i latest arrival

--- lib/widgets/product/latest_arrival.dart
+++ lib/widgets/product/latest_arrival.dart
@@ -10,62 +10,19 @@ import 'package:parfimerija_app/widgets/title_text.dart';
 import 'package:parfimerija_app/providers/theme_providers.dart';
 import 'package:provider/provider.dart';
 
-/*final List<Map<String, String>> latestArrivals = [
-  {
-    "title": "Dior J'adore",
-    "brand": "Dior",
-    "price": "18.500",
-    "image": "https://img.lojasrenner.com.br/item/500891161/zoom/11.jpg",
-    "desc": "A sweet and seductive fragrance with hints of caramel.",
-  },
-  {
-    "title": "Mugler Angel",
-    "brand": "Mugler",
-    "price": "14.000",
-    "image": "https://cdn.tiramisuerp.com/s3.drogerija.me/972935_3529820.jpg",
-    "desc": "A sweet and seductive fragrance with hints of caramel.",
-  },
-  {
-    "title": "Elizabeth Arden Believe",
-    "brand": "Elizabeth Arden",
-    "price": "9.500",
-    "image":
-        "https://www.yourpharmacy.ie/cdn/shop/files/PER0245_1.png?v=1761826235&width=2048",
-        "desc": "A sweet and seductive fragrance with hints of caramel.",
-  },
-  {
-    "title": "Marc Jacobs Daisy",
-    "brand": "Marc Jacobs",
-    "price": "16.000",
-    "image":
-        "https://the-fragrance-shop.imgix.net/products/10968--new--.jpg?fm=webp",
-        "desc": "A sweet and seductive fragrance with hints of caramel."
-  },
-  {
-    "title": "Gucci Bloom",
-    "brand": "Gucci",
-    "price": "19.000",
-    "image":
-        "https://www.perfumestudiomnl.com/cdn/shop/files/image_28839bf4-767f-4aa8-9f36-0523330842d3.png?v=1686755674&width=1946",
-        "desc": "A sweet and seductive fragrance with hints of caramel.",
-  },
-];*/
 
 class LatestArrivalProductsWidget extends StatelessWidget {
   /*final int index;*/
   final Product product;
   const LatestArrivalProductsWidget({super.key, required this.product});
-  
-  
+
   @override
   Widget build(BuildContext context) {
-    
     //final product = latestArrivals[index];
     Size size = MediaQuery.of(context).size;
 
     return GestureDetector(
       onTap: () {
-        
         Navigator.push(
           context,
           MaterialPageRoute(
@@ -98,7 +55,7 @@ class LatestArrivalProductsWidget extends StatelessWidget {
               child: FancyShimmerImage(
                 imageUrl: product.imageUrl,
                 width: double.infinity,
-                height: size.width * 0.6, //ovde povecavm sliku
+                height: size.width * 0.45, //ovde povecavm sliku
                 boxFit: BoxFit.cover,
               ),
             ),
@@ -115,7 +72,7 @@ class LatestArrivalProductsWidget extends StatelessWidget {
                     color: Theme.of(context).textTheme.titleLarge?.color,
                   ),
 
-                  const SizedBox(height: 6),
+                  const SizedBox(height: 5),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
@@ -124,7 +81,14 @@ class LatestArrivalProductsWidget extends StatelessWidget {
                         fontWeight: FontWeight.w600,
                         color: Theme.of(context).textTheme.titleLarge?.color,
                       ),
-                      HeartButtonWidget(size: 22, bkgColor: Colors.transparent),
+
+                      Provider<Product>.value(
+                        value: product,
+                        child: const HeartButtonWidget(
+                          size: 20,
+                          bkgColor: Colors.transparent,
+                        ),
+                      ),
                     ],
                   ),
                 ],
